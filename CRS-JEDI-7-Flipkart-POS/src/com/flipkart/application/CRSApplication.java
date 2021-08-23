63f7ec9c320c0a879a09558cf985d63d0f1646d1
Bharat Ranjan <branjan121@gmail.com>
2021-08-23 22:00:05 +0530

    Application Class Changes

--- CRS-JEDI-7-Flipkart-POS/src/com/flipkart/application/CRSApplication.java
+++ CRS-JEDI-7-Flipkart-POS/src/com/flipkart/application/CRSApplication.java
@@ -3,5 +3,23 @@ package com.flipkart.application;
 public class CRSApplication {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Course Registration System");
+		mainMenu();
+	}
+	public static void mainMenu()
+	{
+		System.out.println("---------Menu-----------");
+		System.out.println("1. Login");
+		System.out.println("2. Change Password");
+		System.out.println("3. Student Registration");
+		System.out.println("4. Exit");
+	}
+	public void loginUser() {
+		
+	}
+	public void registerUser() {
+		
+	}
+	public void changePassword() {
+		
 	}
 }
