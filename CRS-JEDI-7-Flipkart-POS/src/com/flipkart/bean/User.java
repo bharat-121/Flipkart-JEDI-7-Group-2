5a9ececdc97a916d27a85128be0482cb05d10fd0
Kishan Roy <kishanroy360@gmail.com>
2021-08-23 16:22:38 +0530

    added folder structure

---
+++ CRS-JEDI-7-Flipkart-POS/src/com/flipkart/bean/User.java
@@ -0,0 +1,11 @@
+package com.flipkart.bean;
+
+public class User {
+    private String userID;
+    private String name;
+    private String role;
+    private String password;
+    private String phone;
+    private String email;
+    
+}
