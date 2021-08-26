be8d69853bc935935b35de47ca9e58723447c3c2
Kishan Roy <34896443+kishanroy360@users.noreply.github.com>
2021-08-26 14:56:47 +0530

    registration exp

--- CRS-JEDI-7-Flipkart-POS/src/com/flipkart/business/RegistartionInterface.java
+++ CRS-JEDI-7-Flipkart-POS/src/com/flipkart/business/RegistartionInterface.java
@@ -1,6 +1,9 @@
 package com.flipkart.business;
 
 import com.flipkart.bean.Course;
+import com.flipkart.exception.CourseLimitExceedException;
+import com.flipkart.exception.CourseNotFoundException;
+import com.flipkart.exception.SeatNotAvailableException;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -12,7 +15,7 @@ public interface RegistartionInterface {
      * @param studentId
      * @return boolean indicating if the course is added successfully
      */
-    public boolean addCourse(String courseCode, String studentId, List<Course> registeredCourseList) throws Exception;
+    public boolean addCourse(String courseCode, String studentId, List<Course> registeredCourseList) throws CourseNotFoundException, CourseLimitExceedException, SeatNotAvailableException, SQLException;
 
     /**
      *  Method to drop Course selected by student
@@ -21,9 +24,9 @@ public interface RegistartionInterface {
      * @param registeredCourseList
      * @return boolean indicating if the course is dropped successfully
      */
-    public boolean dropCourse(String courseCode, String studentId, List<Course> registeredCourseList) throws Exception;
+    public boolean dropCourse(String courseCode, String studentId, List<Course> registeredCourseList) throws CourseNotFoundException, SQLException;
 
-    public List<Course> viewCourses(String studentId) throws Exception;
+    public List<Course> viewCourses(String studentId) throws SQLException;
 
     /** Method for Fee Calculation for selected courses
      * Fee calculation for selected courses
@@ -37,13 +40,13 @@ public interface RegistartionInterface {
      * @param studentId
      * @return boolean indicating if the student's registration status
      */
-    boolean getRegistrationStatus(String studentId) throws SQLException;
+    public boolean getRegistrationStatus(String studentId) throws SQLException;
 
     /**
      * Method to set student registration status
      * @param studentId
      */
-    void setRegistrationStatus(String studentId) throws SQLException;
+    public void setRegistrationStatus(String studentId) throws SQLException;
 
-    List<Course> viewRegisteredCourses(String studentId) throws SQLException;
+    public List<Course> viewRegisteredCourses(String studentId) throws SQLException;
 }
